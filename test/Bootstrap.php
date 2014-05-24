0d0349931ca9b803911e81546e2c971560fd4ad3
Designmoves <github@designmoves.nl>
2014-05-25 00:33:11 +0200

    Updated CS-fixer config and Bootstrap namespace

--- test/Bootstrap.php
+++ test/Bootstrap.php
@@ -29,7 +29,7 @@
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-namespace DesignmovesStrictViewVarsTest;
+namespace DesignmovesStrictViewVars\test;
 
 use Zend\Loader\AutoloaderFactory;
 use Zend\Mvc\Service\ServiceManagerConfig;
